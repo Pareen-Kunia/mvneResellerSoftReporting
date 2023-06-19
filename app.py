5c49d5769255e4d7a2c13bef167da739fcf9cc59
Pareen Kunia <pareen.kunia@gmail.com>
2023-06-19 19:07:11 +1200

    mvNE

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import requests
 import json
 from urllib.parse import urlencode
 from datetime import datetime
+import logging
+from logging import StreamHandler
+from datadog_api_client.v2 import ApiClient, ApiException, Configuration
+from datadog_api_client.v2.api import logs_api
+from datadog_api_client.v2.models import *
 
 load_dotenv()
 
@@ -14,33 +19,78 @@ USER_NAME = os.getenv('USER_NAME')
 PASSWORD = os.getenv('PASSWORD')
 AUTH_SPID = os.getenv('AUTH_SPID')
 SPID = os.getenv('SPID')
+XVNE_API_KEY = os.getenv('XVNE_API_KEY')
+PLAN_VALUES = []
+LOG_VALUES = []
 
-JWT = ""
+class DDHandler(StreamHandler):
+    def __init__(self, configuration, service_name, ddsource):
+        StreamHandler.__init__(self)
+        self.configuration = configuration
+        self.service_name = service_name
+        self.ddsource = ddsource
+ 
+    def emit(self, record):
+        msg = self.format(record)
+ 
+        with ApiClient(self.configuration) as api_client:
+            api_instance = logs_api.LogsApi(api_client)
+            body = HTTPLog(
+                [
+                    HTTPLogItem(
+                        ddsource=self.ddsource,
+                        ddtags="env:{}".format(
+                            os.getenv("ENV"),
+ 
+                        ),
+                        message=msg,
+                        service=self.service_name,
+                    ),
+                ]
+            )
+ 
+            try:
+                # Send logs
+                
+                api_response = api_instance.submit_log(body)
+                
+            except ApiException as e:
+                print("Exception when calling LogsApi->submit_log: %s\n" % e)
 
-def Auth():
-    AUTH_URL = "{}{}{}".format(BASE_URL,AUTH_SPID,"/auth_check")
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-    
-    jsonData = {
-        "IdentityProvider": "LOCAL",
-        "AuthID": USER_NAME,
-        "AuthCode": PASSWORD,
-        "IncludeRoles": True
-    }
-    
-    r = requests.post(AUTH_URL,data=json.dumps(jsonData),headers=headers)
-    
-    resp = r.json()
-    JWT = resp.get("JWT")
-    return JWT
-
+def push2Datadog(service_name,ddsource,data):
+    configuration = Configuration()
+    with ApiClient(configuration) as api_client:
+            api_instance = logs_api.LogsApi(api_client)
+            body = HTTPLog(
+                [
+                    HTTPLogItem(
+                        ddsource=ddsource,
+                        ddtags="env:{}".format(
+                            os.getenv("ENV"),
+ 
+                        ),
+                        message=json.dumps(data),
+                        service=service_name,
+                    ),
+                ]
+            )
+    try:
+        # Send logs
+        api_response = api_instance.submit_log(body)
+        
+    except ApiException as e:
+        print("Exception when calling LogsApi->submit_log: %s\n" % e)
+                                
 def getAllServices():
-    JWT = Auth()
+   
     
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json','X-Auth-Token':JWT}
-    params = {'SPID':SPID,"SearchOptions.PageSize":100}
+    getAllPlans()
     
-    SERVICE_URL = "{}{}?{}".format(BASE_URL,"v4/mobile/addons",urlencode(params))
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json','x-api-key':XVNE_API_KEY}
+    params = {'SearchOptions.PageNumber':1,"SearchOptions.PageSize":100,"SearchOptions.IndexStart":0,"SPID":SPID,"IncludeDeleted":False}
+    
+    
+    SERVICE_URL = "{}{}?{}".format(BASE_URL,"v4/mobiles",urlencode(params))
     
     
     
@@ -48,35 +98,57 @@ def getAllServices():
     resp = r.json()
     
     totalCount = resp.get("PageInfo")["TotalItemCount"]
+    _data = {
+            "Total": totalCount
+        }
+   
+    push2Datadog('xvnestpmreportingsvc','app.py',_data)
     
-    MobileAddons =  resp.get("MobileAddons")
+    Mobiles =  resp.get("Mobiles")
     print("Account ID,title,first_name,last_name,full_name,email_address,mobile_number,product_name,product_activation_date")
-    for Addon in MobileAddons:
-        CustomerUUID = Addon.get("CustomerUUID")
-        MobileUUID = Addon.get("MobileUUID")
-        Service = getService(JWT,MobileUUID)
-        getAllCustomers(JWT,CustomerUUID,Addon,Service)
+    for Mobile in Mobiles:
+        CustomerUUID = Mobile.get("CustomerUUID")
+        getAllCustomers(CustomerUUID,Mobile)
         
-    print(totalCount)
         
-def getService(JWT, UUID):
+        
+        
+# def getAllServices(JWT):
     
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json','X-Auth-Token':JWT}
-    params = {'SPID':SPID,"UUID":UUID}
+#     headers = {'Content-type': 'application/json', 'Accept': 'application/json','x-api-key':JWT}
+#     params = {'SearchOptions.PageNumber':1,"SearchOptions.PageSize":100,"SearchOptions.IndexStart":0,"SPID":SPID,"IncludeDeleted":False}
     
-    SERVICE_URL = "{}{}?{}".format(BASE_URL,"v4/mobile",urlencode(params))
-    r = requests.get(SERVICE_URL,headers=headers)
+#     SERVICE_URL = "{}{}?{}".format(BASE_URL,"v4/mobile",urlencode(params))
+#     r = requests.get(SERVICE_URL,headers=headers)
+#     resp = r.json()
+    
+    
+    
+    
+#     return resp
+    
+def getAllPlans():
+    
+    
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json','x-api-key':XVNE_API_KEY}
+    params = {'SPID':68}
+    PLANS_URL = "{}{}{}?{}".format(BASE_URL,"v4/","mobile/addon_plans",urlencode(params))
+    
+    
+    r = requests.get(PLANS_URL,headers=headers)
     resp = r.json()
     
+    MobileAddonPlans = resp.get("MobileAddonPlans")
+    
+    for Addon in MobileAddonPlans:
+        
+        PLAN_VALUES.append(Addon)
     
     
+def getAllCustomers(CustomerUUID,Mobile):
     
-    return resp
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json','x-api-key':XVNE_API_KEY}
     
-
-def getAllCustomers(JWT,CustomerUUID,Addon,Service):
-    
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json','X-Auth-Token':JWT}
     CUSTOMER_URL = "{}{}/{}{}{}".format(BASE_URL,"v2",SPID,"/customer/",CustomerUUID)
     
     r = requests.get(CUSTOMER_URL,headers=headers)
@@ -85,12 +157,25 @@ def getAllCustomers(JWT,CustomerUUID,Addon,Service):
     LastName = resp.get("LastName")
     Email = resp.get("Email")
     
-    createdDate = Addon.get("CreatedAtNanos")
+    createdDate = Mobile.get("CreatedAtNanos")
     secs = createdDate / 1e9
     dt = datetime.fromtimestamp(secs)
     
+    plan = ""
     
-    print("{},{},{},{},{},{},{},{},{}".format(CustomerUUID,"",FirstName,LastName,"{} {}".format(FirstName,LastName),Email,Service.get("MSISDN"),Addon.get("Name"),dt.strftime('%Y-%m-%dT%H:%M:%S.%f')))
+    if Mobile.get('Addons') is not None:
+        #print(Mobile.get('Addons')[0])
+        for item in PLAN_VALUES:
+            if item['ProductAvailabilityUUID'] == Mobile.get('Addons')[0]['AddonAvailabilityUUID']:
+                plan = item.get('Name')
+                
+                push2Datadog('xvnestpmreportingsvc','app.py',{"stpmPlanName":plan,"stpmPlanCount":1,"stpmPlanTimeStamp":dt.strftime('%Y-%m-%dT%H:%M:%S.%f')})
+                print("{},{},{},{},{},{},{},{},{}".format(CustomerUUID,"",FirstName,LastName,"{} {}".format(FirstName,LastName),Email,Mobile.get("MSISDN"),plan,dt.strftime('%Y-%m-%dT%H:%M:%S.%f')))
         
+        
+
+
+
+
 getAllServices()
     
